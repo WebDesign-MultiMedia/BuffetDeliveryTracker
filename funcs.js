4dc51d6a7bcfea687ef33e61a160f15371128ff7
Julio <youremail@example.com>
2025-01-12 01:18:47 -0500

    API fixed

--- funcs.js
+++ funcs.js
@@ -1,13 +1,3 @@
-// const Swal = require('sweetalert2')
-
-// Swal.fire({
-//     title: 'Error!',
-//     text: 'Do you want to continue',
-//     icon: 'error',
-//     confirmButtonText: 'Cool'
-// })
-
-
 const title = document.getElementById('topContainer');
 const imgLogo = document.getElementById('logo');
 const cusform = document.getElementById('CustomerDetails');
@@ -24,7 +14,7 @@ showElement(imgLogo, 4000);
 showElement(topBtn, 6000);
 
 // BOTTON
-
+const body = document.getElementById('bodyPg');
 const btn  = document.getElementById('letsTrack');
 btn.addEventListener('click', () =>{
 
@@ -32,5 +22,54 @@ btn.addEventListener('click', () =>{
     title.style.display = 'none';
     cusform.style.display = 'flex';
     cusform.style.justifyContent = 'center';
-    
+    body.style.background = 'black';
 })
+
+// SUBMIT FORM 
+document.addEventListener("DOMContentLoaded", ()=>{
+const smtBtn = document.getElementById("sbBtn");
+smtBtn.addEventListener("click", ()=>{
+
+    event.preventDefault();
+
+    async function gSheetSubmit() {
+        const url = "https://sheetdb.io/api/v1/knvxv3inyom8t";
+        const data = {
+            Date : document.getElementById("Date").value ,
+            Name : document.getElementById("Name").value,
+            Contact : document.getElementById('Contact').value,
+            Address : document.getElementById('Address').value,
+            Items : document.getElementById('Items').value,
+            Quantities : document.getElementById('Quantities').value,
+            Requests : document.getElementById('Requests').value,
+            Cost : document.getElementById('Cost').value,
+            Payment : document.getElementById('Payment').value
+        };
+        try{
+            const res = await fetch(url, {
+                method: "POST",
+                body: JSON.stringify(data),
+                headers: {
+                    "Content-Type" : "application/json",
+                },
+            });
+            if(!res.ok){
+                throw new Error(`Response status: ${res.status}`);
+            }
+            const json = await res.json();
+            console.log(json);
+        } catch(err){
+            console.error(err.message);
+            
+        }
+    }
+
+    gSheetSubmit();
+
+    // swal("Here's the title!", "...and here's the text!");
+
+    setTimeout(() => {
+        location.reload(); 
+     }, 3000);
+});
+});
